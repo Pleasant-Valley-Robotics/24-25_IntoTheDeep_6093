ca135470ce098ecee0a49ddbe32e675fcd6acfdd
Will Rages <rageswill@pleasval.org>
2024-10-24 18:45:25 -0500

    add wheel constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.kt
@@ -1,7 +1,16 @@
 package org.firstinspires.ftc.teamcode
 
+import android.webkit.WebMessage
+import kotlin.math.PI
+
 object DriveConstants {
-    const val ENCODER_PER_INCH = 0.0
+    private const val WHEEL_DIAMETER = 4
+
+    // NeveRest 20 gearmotors with a 7:9 gearing on top
+    // note that NeveRest 20s are actually a 19.2:1 gear ratio
+    // https://www.andymark.com/products/neverest-orbital-20-gearmotor
+    private const val TICKS_PER_REV = 537.6 * 7 / 9
+    const val ENCODER_PER_INCH = TICKS_PER_REV / (WHEEL_DIAMETER * PI)
     const val STRAFING_CORRECTION = 1.1
 }
 
