6a399106dc5d11f126dbf862adbaed48294a1a49
Will Rages <rageswill@pleasval.org>
2024-10-24 18:46:25 -0500

    update example auto to copy radical's simple pushbot auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ExampleAuto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ExampleAuto.kt
@@ -20,13 +20,35 @@ class ExampleAuto : LinearOpMode() {
 
         runBlocking {
             val auto = launch {
-                coroutineScope {
-                    drivebase.driveForward(10.0, 1.0)
-                    drivebase.strafeRight(10.0, 1.0)
-                }
-                coroutineScope {
-                    launch { drivebase.strafeRight(10.0, 1.0) }
-                    launch { drivebase.driveForward(10.0, 1.0) }
+                val driveSpeed = 0.5
+                val turnSpeed = 0.5
+
+                with(drivebase) {
+                    strafeRight(4.0, driveSpeed)
+                    turnToAngle(-15.0, turnSpeed)
+                    driveForward(22.0, driveSpeed)
+                    turnToAngle(45.0, turnSpeed)
+                    driveForward(3.0, driveSpeed)
+
+                    driveForward(-30.0, driveSpeed)
+                    turnToAngle(90.0, turnSpeed)
+                    driveForward(-27.0, driveSpeed)
+                    turnToAngle(0.0, turnSpeed)
+                    driveForward(5.0, driveSpeed)
+                    turnToAngle(75.0, turnSpeed)
+                    driveForward(50.0, driveSpeed)
+
+                    driveForward(-46.0, driveSpeed)
+                    turnToAngle(0.0, turnSpeed)
+
+                    driveForward(13.0, driveSpeed)
+                    turnToAngle(90.0, turnSpeed)
+                    driveForward(42.0, driveSpeed)
+
+                    driveForward(-4.0, driveSpeed)
+                    turnToAngle(180.0, turnSpeed)
+                    driveForward(112.0, driveSpeed)
+                    strafeRight(12.0, driveSpeed)
                 }
             }
 
@@ -36,6 +58,9 @@ class ExampleAuto : LinearOpMode() {
 
                 yield()
             }
+
+            // stop motors
+            drivebase.controlMotors(0.0, 0.0, 0.0)
 
             auto.cancelAndJoin()
         }
