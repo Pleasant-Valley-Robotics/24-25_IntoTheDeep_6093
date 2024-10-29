a46e6253cec4a3896a24b15fbf6d8599b5a0c81d
Will Rages <rageswill@pleasval.org>
2024-10-29 17:08:07 -0500

    update constants for new robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/Constants.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/Constants.kt
@@ -2,13 +2,22 @@ package org.firstinspires.ftc.teamcode.utility
 
 import kotlin.math.PI
 
-object DriveConstants {
-    private const val WHEEL_DIAMETER = 4
+// note that we should be using inches, seconds, and degrees unless specified otherwise
 
+object DriveConstants {
+    // dan wheel diameter and ticks
+    // private const val WHEEL_DIAMETER = 4
     // NeveRest 20 gearmotors with a 7:9 gearing on top
-    // note that NeveRest 20s are actually a 19.2:1 gear ratio
     // https://www.andymark.com/products/neverest-orbital-20-gearmotor
-    private const val TICKS_PER_REV = 537.6 * 7 / 9
+    // private const val TICKS_PER_REV = 537.6 * 7 / 9
+
+    // gobilda 144mm wheels as inches
+    private const val WHEEL_DIAMETER = 144 * (0.1 / 2.54)
+
+    // https://www.gobilda.com/5203-series-yellow-jacket-planetary-gear-motor-26-9-1-ratio-24mm-length-8mm-rex-shaft-223-rpm-3-3-5v-encoder/
+    // PPR at output shaft, no gear ratio
+    private const val TICKS_PER_REV = 751.8
+
     const val ENCODER_PER_INCH = TICKS_PER_REV / (WHEEL_DIAMETER * PI)
     const val STRAFING_CORRECTION = 1.1
 }
