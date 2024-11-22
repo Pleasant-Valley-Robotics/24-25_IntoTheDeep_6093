acea668108414c486d76af7a0970fed035f86745
Will Rages <rageswill@pleasval.org>
2024-11-21 18:12:23 -0600

    more accurate lift constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/Constants.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/Constants.kt
@@ -4,6 +4,8 @@ import kotlin.math.PI
 
 // note that we should be using inches, seconds, and degrees unless specified otherwise
 
+private const val BASE_PPR = 28
+
 object DriveConstants {
     // dan wheel diameter and ticks
     // private const val WHEEL_DIAMETER = 4
@@ -11,20 +13,26 @@ object DriveConstants {
     // https://www.andymark.com/products/neverest-orbital-20-gearmotor
     // private const val TICKS_PER_REV = 537.6 * 7 / 9
 
-    // gobilda 144mm wheels as inches
-    private const val WHEEL_DIAMETER = 144 * (0.1 / 2.54)
+    // 5203 gobilda 19.2:1s
+    private const val TICKS_PER_REV = 19.20320855614973 * BASE_PPR
 
-    // https://www.gobilda.com/5203-series-yellow-jacket-planetary-gear-motor-26-9-1-ratio-24mm-length-8mm-rex-shaft-223-rpm-3-3-5v-encoder/
-    // PPR at output shaft, no gear ratio
-    private const val TICKS_PER_REV = 751.8
+    // gobilda 140mm wheels as inches
+    private const val WHEEL_DIAMETER = 140 * (0.1 / 2.54)
+
 
     const val ENCODER_PER_INCH = TICKS_PER_REV / (WHEEL_DIAMETER * PI)
     const val STRAFING_CORRECTION = 1.1
 }
 
 object LiftConstants {
-    const val ENCODER_PER_INCH = 0.0
-    const val MAX_LIFT_HEIGHT_INCH = 0.0
+    // 5203 gobilda 26.9:1s
+    private const val TICKS_PER_REV = 26.85123966942149 * BASE_PPR
+
+    // 2mm pitch gt2 hub mount, 38.2 pitch diameter
+    private const val PITCH_DIAMETER = 38.2 * (0.1 / 2.54)
+
+    const val ENCODER_PER_INCH = TICKS_PER_REV / (PITCH_DIAMETER * PI)
+    const val MAX_LIFT_HEIGHT_INCH = 15.0
 }
 
 object PrototypeConstants {
