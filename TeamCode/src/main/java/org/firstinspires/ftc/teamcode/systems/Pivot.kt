281e6ec5eaff1172d2c8da279bf02a6d866a6bd1
Will Rages <rageswill@pleasval.org>
2024-11-23 09:51:32 -0600

    pivot is an unused class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/Pivot.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/Pivot.kt
@@ -3,18 +3,16 @@ package org.firstinspires.ftc.teamcode.systems
 import com.qualcomm.robotcore.hardware.HardwareMap
 import com.qualcomm.robotcore.hardware.Servo
 import org.firstinspires.ftc.robotcore.external.Telemetry
-import org.firstinspires.ftc.teamcode.utility.PrototypeConstants
 
 class Pivot(hardwareMap: HardwareMap) {
 
-    private val MAX_HEIGHT = PrototypeConstants.MAX_LIFT_HEIGHT
 
     private val pivotServo = hardwareMap.servo.get("Pivot").apply {
         this.direction = Servo.Direction.REVERSE
     }
 
     fun runToAngle(position: Double) {
-        var targetPivot = position.coerceIn(0.0, MAX_HEIGHT)
+        var targetPivot = position.coerceIn(0.0, 1.0)
         if(position < 0.0488) {
             targetPivot = 0.0488
         }
