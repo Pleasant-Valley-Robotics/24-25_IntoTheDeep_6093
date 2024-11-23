a27fa9ef95346593cde5c33d5467770aa68e0fe0
Will Rages <rageswill@pleasval.org>
2024-11-23 09:50:08 -0600

    clear ExampleAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ExampleAuto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ExampleAuto.kt
@@ -19,36 +19,7 @@ class ExampleAuto : LinearOpMode() {
 
         runBlocking {
             val auto = launch {
-                val driveSpeed = 0.5
-                val turnSpeed = 0.5
-
-                with(drivebase) {
-                    strafeRight(4.0, driveSpeed)
-                    turnToAngle(-15.0, turnSpeed)
-                    driveForward(22.0, driveSpeed)
-                    turnToAngle(45.0, turnSpeed)
-                    driveForward(3.0, driveSpeed)
-
-                    driveForward(-30.0, driveSpeed)
-                    turnToAngle(90.0, turnSpeed)
-                    driveForward(-27.0, driveSpeed)
-                    turnToAngle(0.0, turnSpeed)
-                    driveForward(5.0, driveSpeed)
-                    turnToAngle(75.0, turnSpeed)
-                    driveForward(50.0, driveSpeed)
-
-                    driveForward(-46.0, driveSpeed)
-                    turnToAngle(0.0, turnSpeed)
-
-                    driveForward(13.0, driveSpeed)
-                    turnToAngle(90.0, turnSpeed)
-                    driveForward(42.0, driveSpeed)
-
-                    driveForward(-4.0, driveSpeed)
-                    turnToAngle(180.0, turnSpeed)
-                    driveForward(112.0, driveSpeed)
-                    strafeRight(12.0, driveSpeed)
-                }
+                drivebase.turnToAngle(90.0, 0.5)
             }
 
             while (opModeIsActive() && auto.isActive) {
