73a4f47fbf563e90737e27d1467f6a50434d6fdb
Will Rages <rageswill@pleasval.org>
2024-11-23 09:50:26 -0600

    add extension arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/Extender.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/Extender.kt
@@ -4,14 +4,19 @@ import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcore.external.Telemetry
+import org.firstinspires.ftc.teamcode.utility.ExtenderConstants.ENCODER_PER_INCH
 
 class Extender(hardwareMap: HardwareMap) {
     val extendMotor = hardwareMap.dcMotor.get("ExtendMotor")!!.apply {
         this.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
         this.direction = DcMotorSimple.Direction.REVERSE
+        this.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        this.mode = DcMotor.RunMode.RUN_USING_ENCODER
     }
 
+    val extendPosition get() = extendMotor.currentPosition / ENCODER_PER_INCH
+
     fun addTelemetry(telemetry: Telemetry) {
-        telemetry.addData("Extended", extendMotor.currentPosition)
+        telemetry.addData("Extended", extendMotor.currentPosition / ENCODER_PER_INCH)
     }
 }
