04e4c5b22365dc0cfada25398316e7cc717daa4a
Will Rages <rageswill@pleasval.org>
2024-12-21 20:20:20 -0600

    pipeline parameter classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/FilterParams.kt
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.utility
+
+data class FilterParams(
+    val aMin: Double,
+    val aMax: Double,
+    val bMin: Double,
+    val bMax: Double,
+    val aPerB: Double,
+    val bPerA: Double,
+)
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/WorldParams.kt
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.utility
+
+/**
+ * @param targetX units in cm
+ * @param targetY units in cm
+ * @param cameraX units in cm
+ * @param cameraY units in cm
+ * @param cameraZ units in cm
+ * @param cameraXRot units in deg
+ * @param cameraYRot units in deg
+ * @param cameraZRot units in deg
+ * @param imWidth units in px
+ * @param imHeight units in px
+ * @param sensorWidth units in mm
+ * @param focalLength units in mm
+ * @param detectedZ units in cm
+ */
+data class WorldParams(
+    val targetX: Double,
+    val targetY: Double,
+    val cameraX: Double,
+    val cameraY: Double,
+    val cameraZ: Double,
+    val cameraXRot: Double,
+    val cameraYRot: Double,
+    val cameraZRot: Double,
+    val imWidth: Double,
+    val imHeight: Double,
+    val sensorWidth: Double,
+    val focalLength: Double,
+    val detectedZ: Double,
+)
