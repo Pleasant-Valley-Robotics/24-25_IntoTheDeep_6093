7d735207b88327067a171c04da39764c55cf8c61
Will Rages <rageswill16@gmail.com>
2025-01-16 18:36:16 -0800

    adjust block color constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/vision/BlockColor.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/vision/BlockColor.kt
@@ -7,14 +7,14 @@ enum class BlockColor {
     Yellow,
     Blue;
 
-    fun getFilterParams(): FilterParams = when (this) {
+    fun getFilterParams() = when (this) {
         Red -> FilterParams(
             minA = 148,
             maxA = 201,
             minB = 121,
             maxB = 158,
-            aPerB = -0.4116666666666666f,
-            bPerA = 0.6566666666666667f,
+            aPerB = -0.41166666f,
+            bPerA = 0.6566667f,
         )
 
         Yellow -> FilterParams(
@@ -22,8 +22,8 @@ enum class BlockColor {
             maxA = 150,
             minB = 138,
             maxB = 201,
-            aPerB = 0.03500000000000014f,
-            bPerA = 0.2616666666666667f,
+            aPerB = 0.035f,
+            bPerA = 0.26166666f,
         )
 
         Blue -> FilterParams(
@@ -31,8 +31,8 @@ enum class BlockColor {
             maxA = 204,
             minB = 28,
             maxB = 96,
-            aPerB = -0.6416666666666666f, 
-            bPerA = -0.0016666666666664831f,
+            aPerB = -0.64166665f,
+            bPerA = -0.0016666667f,
         )
     }
 }
