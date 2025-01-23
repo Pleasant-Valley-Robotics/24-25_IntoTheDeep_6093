b55f5b4f39156bb9a2d2452f76508f1bc91c4404
Will Rages <rageswill@pleasval.org>
2025-01-23 15:58:37 -0600

    template HighBasketExtendAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/HighBasketExtendAuto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/HighBasketExtendAuto.kt
@@ -2,10 +2,57 @@ package org.firstinspires.ftc.teamcode.opmodes
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
+import org.firstinspires.ftc.teamcode.systems.Bucket
+import org.firstinspires.ftc.teamcode.systems.Drivebase
+import org.firstinspires.ftc.teamcode.systems.Extender
 import org.firstinspires.ftc.teamcode.systems.LeftLift
+import org.firstinspires.ftc.teamcode.systems.Odometry
+import org.firstinspires.ftc.teamcode.systems.Pivot
+import org.firstinspires.ftc.teamcode.systems.Spintake
 
 @Autonomous(name = "HighBasketExtendAuto")
 class HighBasketExtendAuto : LinearOpMode() {
     override fun runOpMode() {
+        telemetry.status("initializing")
+
+        val odometry = Odometry(hardwareMap)
+        val drivebase = Drivebase(hardwareMap, odometry)
+        val lift = LeftLift(hardwareMap)
+        val extender = Extender(hardwareMap)
+
+        val bucket = Bucket(hardwareMap)
+        val spintake = Spintake(hardwareMap)
+        val pivot = Pivot(hardwareMap)
+
+        odometry.resetOdometry()
+        lift.resetLift()
+        extender.resetExtender()
+
+        telemetry.status("initialized")
+
+        waitForStart()
+
+        runBlocking {
+            val auto = launch {  }
+
+            while (opModeIsActive() && auto.isActive) {
+                drivebase.addTelemetry(telemetry)
+                lift.addTelemetry(telemetry)
+                odometry.addTelemetry(telemetry)
+                telemetry.status("running")
+
+                odometry.update()
+                yield()
+            }
+
+            auto.cancelAndJoin()
+
+            drivebase.controlMotors(0.0, 0.0, 0.0)
+            lift.setLiftPowerSafe(0.0, true)
+        }
     }
 }
