1ab2921318343da43794d8136af49d4c318799b9
Will Rages <rageswill@pleasval.org>
2025-01-31 15:53:52 -0600

    change clipper auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ClipperAuto4.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ClipperAuto4.kt
@@ -54,7 +54,7 @@ class ClipperAuto4 : LinearOpMode() {
             suspend fun scorePole(offset: Double) {
                 parallelWait(
                     {
-                        drivebase.driveOffsetGlobal(-8.21 + offset, 15.79, -1.571, 1.0, false)
+                        drivebase.driveOffsetGlobal(-8.21 + offset, 15.79, -1.5, 1.0, false)
                         drivebase.driveOffsetGlobal(-8.21 + offset, 25.99, -1.571, 0.75, true)
                     },
                     { lift.moveLiftTo(MAX_LIFT_HEIGHT_RIGHT - 2.0) },
@@ -95,7 +95,7 @@ class ClipperAuto4 : LinearOpMode() {
                 scorePole(-1.0)
 
                 parallelWait({
-                    drivebase.driveOffsetGlobal(21.95, 5.879, 1.571, 1.0, false)
+                    drivebase.driveOffsetGlobal(21.95, 5.879, 1.5, 1.0, false)
                     drivebase.driveOffsetGlobal(21.95, 1.014, 1.571, 1.0, false)
                 },
                     { lift.moveLiftTo(0.0) }
@@ -107,7 +107,7 @@ class ClipperAuto4 : LinearOpMode() {
                 scorePole(2.0)
 
                 parallelWait({
-                    drivebase.driveOffsetGlobal(21.95, 5.879, 1.571, 1.0, false)
+                    drivebase.driveOffsetGlobal(21.95, 5.879, 1.5, 1.0, false)
                     drivebase.driveOffsetGlobal(21.95, 1.014, 1.571, 1.0, false)
                 },
                     { lift.moveLiftTo(0.0) }
@@ -119,15 +119,15 @@ class ClipperAuto4 : LinearOpMode() {
 
                 parallelWait(
                     {
-                        drivebase.driveOffsetGlobal(-8.21 + 5.0, 15.79, -1.571, 1.0, false)
-                        drivebase.driveOffsetGlobal(-8.21 + 5.0, 25.99, -1.571, 0.75, true)
+                        drivebase.driveOffsetGlobal(-8.21 + 5.0, 15.79, -1.5, 1.0, false)
+                        drivebase.driveOffsetGlobal(-8.21 + 5.0, 26.39, -1.571, 0.5, true)
                     },
                     { lift.moveLiftTo(MAX_LIFT_HEIGHT_RIGHT - 2.25) },
                 )
 
                 clipper.moveClaw(Clipper.ClipperState.Open)
                 delay(100L)
-                drivebase.driveOffsetGlobal(21.95, 1.014, 1.571, 1.0, false)
+                drivebase.driveOffsetGlobal(21.95, 1.014, -1.571, 1.0, false)
             }
 
             while (opModeIsActive() && auto.isActive) {
